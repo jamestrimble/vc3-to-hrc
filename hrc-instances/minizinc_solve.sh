c0168c98029a83be1fa01e7c8c8e261fa341f53b
James Trimble <james.trimble@yahoo.co.uk>
2016-10-11 23:10:37 +0100

    Added script to solve an HRC instance using MiniZinc

---
+++ hrc-instances/minizinc_solve.sh
@@ -0,0 +1,7 @@
+if [ -f $1.hrc ]
+then
+  python ../../hrc-minizinc/hrc-to-minizinc.py 0 --no-presolve < $1.hrc > $1.dzn
+  minizinc ../../hrc-minizinc/hrc.mzn $1.dzn
+else
+  echo "Usage: $0 INSTANCE    (omit .hrc extension)"
+fi
