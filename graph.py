c5afeb20b487201586d6a16f4fb1a666c0833120
James Trimble <james.trimble@yahoo.co.uk>
2016-10-11 22:38:49 +0100

    Moved some code from graph.py to min_vc.py

--- graph.py
+++ graph.py
@@ -26,28 +26,3 @@ def read(fname):
         g.add_edge(*e)
     return g
     
-############ Min vertex cover #################
-
-def covers_vertices(vv, g):
-    for i, j in edge_iter(g):
-        if i not in vv and j not in vv:
-            return False
-    return True
-
-def vc(g, i, vv):
-    if len(vv) == i:
-        return covers_vertices(vv, g)
-    elif len(vv) == 0:
-        for j in range(g.n):
-            if vc(g, i, [j]): return True
-    else:
-        for j in range(vv[-1]+1, g.n):
-            if  vc(g, i, vv+[j]): return True
-    return False
-
-def min_vc(g):
-    for i in range(g.n+1):
-        if vc(g, i, []):
-            return i
-
-##############################################
--- min_vc.py
+++ min_vc.py
@@ -2,8 +2,29 @@ import sys
 
 import graph
 
+def covers_vertices(vv, g):
+    for i, j in graph.edge_iter(g):
+        if i not in vv and j not in vv:
+            return False
+    return True
+
+def vc(g, i, vv):
+    if len(vv) == i:
+        return covers_vertices(vv, g)
+    elif len(vv) == 0:
+        for j in range(g.n):
+            if vc(g, i, [j]): return True
+    else:
+        for j in range(vv[-1]+1, g.n):
+            if  vc(g, i, vv+[j]): return True
+    return False
+
+def min_vc(g):
+    for i in range(g.n+1):
+        if vc(g, i, []):
+            return i
+
 if __name__=="__main__":
     fname = sys.argv[1]
     g = graph.read(fname)
-    g.show()
-    print graph.min_vc(g)
+    print min_vc(g)
