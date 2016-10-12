302d62bbfa29242a836b1266f4348d89587bbce7
James Trimble <james.trimble@yahoo.co.uk>
2016-10-12 14:52:23 +0100

    More verbose output from test program

--- vc_hrc_vs_brute_force.sh
+++ vc_hrc_vs_brute_force.sh
@@ -1,6 +1,7 @@
 if [ -f $1.graph ]
 then
   MINVC=$(python min_vc.py $1.graph)
+  echo Size of min vertex cover for $1.graph: $MINVC
   python vc3-to-hrc.py $1.graph $MINVC > tmp/tmp.hrc
   ./minizinc_solve.sh tmp/tmp
   if [ $? -ne 0 ]; then echo "$1 satisfiable instance failed"; fi
