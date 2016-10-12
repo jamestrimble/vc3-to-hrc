210583e97f6d1a32e3d4bdab4398be24ff9739ed
James Trimble <james.trimble@yahoo.co.uk>
2016-10-12 14:42:50 +0100

    Switched on presolve in minizinc_solve.sh

--- minizinc_solve.sh
+++ minizinc_solve.sh
@@ -1,6 +1,6 @@
 if [ -f $1.hrc ]
 then
-  python ../hrc-minizinc/hrc-to-minizinc.py 0 --no-presolve < $1.hrc > $1.dzn && \
+  python ../hrc-minizinc/hrc-to-minizinc.py 0 < $1.hrc > $1.dzn && \
   minizinc ../hrc-minizinc/hrc.mzn $1.dzn | grep UNSATISFIABLE
   exit $((1-$?))
 else
