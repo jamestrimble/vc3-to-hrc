a5e96f232455c029cd548023c461893514c5a38d
James Trimble <james.trimble@yahoo.co.uk>
2016-10-12 14:05:19 +0100

    Added script to test HRC reduction

--- minizinc_solve.sh
+++ minizinc_solve.sh
@@ -1,7 +1,8 @@
 if [ -f $1.hrc ]
 then
-  python ../../hrc-minizinc/hrc-to-minizinc.py 0 --no-presolve < $1.hrc > $1.dzn && \
-  minizinc ../../hrc-minizinc/hrc.mzn $1.dzn
+  python ../hrc-minizinc/hrc-to-minizinc.py 0 --no-presolve < $1.hrc > $1.dzn && \
+  minizinc ../hrc-minizinc/hrc.mzn $1.dzn | grep UNSATISFIABLE
+  exit $((1-$?))
 else
   echo "Usage: $0 INSTANCE    (omit .hrc extension)"
 fi
---
+++ vc_hrc_vs_brute_force.sh
@@ -0,0 +1,14 @@
+if [ -f $1.graph ]
+then
+  MINVC=$(python min_vc.py $1.graph)
+  python vc3-to-hrc.py $1.graph $MINVC > tmp/tmp.hrc
+  ./minizinc_solve.sh tmp/tmp
+  if [ $? -ne 0 ]; then echo "$1 satisfiable instance failed"; fi
+  if [ $MINVC -gt 0 ]; then
+    python vc3-to-hrc.py $1.graph $(($MINVC-1)) > tmp/tmp.hrc
+    ./minizinc_solve.sh tmp/tmp > /dev/null
+    if [ $? -ne 1 ]; then echo "$1 unsatisfiable instance failed"; fi
+  fi
+else
+  echo "Usage: $0 INSTANCE    (omit .hrc extension)"
+fi
