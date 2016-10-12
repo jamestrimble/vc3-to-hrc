cf5e36635546fa835ce20b4e6ef596927e772141
James Trimble <james.trimble@yahoo.co.uk>
2016-10-12 11:56:35 +0100

    Fail early in minizinc_solve.sh

--- hrc-instances/minizinc_solve.sh
+++ hrc-instances/minizinc_solve.sh
@@ -1,6 +1,6 @@
 if [ -f $1.hrc ]
 then
-  python ../../hrc-minizinc/hrc-to-minizinc.py 0 --no-presolve < $1.hrc > $1.dzn
+  python ../../hrc-minizinc/hrc-to-minizinc.py 0 --no-presolve < $1.hrc > $1.dzn && \
   minizinc ../../hrc-minizinc/hrc.mzn $1.dzn
 else
   echo "Usage: $0 INSTANCE    (omit .hrc extension)"
