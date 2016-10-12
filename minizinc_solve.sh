1ded282ed0457e6c544b6b1a4d9bd317d90388a8
James Trimble <james.trimble@yahoo.co.uk>
2016-10-12 14:55:09 +0100

    Changed MiniZinc backend to g12lazy

--- minizinc_solve.sh
+++ minizinc_solve.sh
@@ -1,7 +1,7 @@
 if [ -f $1.hrc ]
 then
   python ../hrc-minizinc/hrc-to-minizinc.py 0 < $1.hrc > $1.dzn && \
-  minizinc ../hrc-minizinc/hrc.mzn $1.dzn | grep UNSATISFIABLE
+  mzn-g12lazy ../hrc-minizinc/hrc.mzn $1.dzn | grep UNSATISFIABLE
   exit $((1-$?))
 else
   echo "Usage: $0 INSTANCE    (omit .hrc extension)"
